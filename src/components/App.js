1a082eb3e5ae6e47809c01a1863bdbe0e3245c22
pianistoff <severyn.h@gmail.com>
2022-05-27 09:00:41 +0300

    Declare font weight in header

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,3 @@
-import "@fontsource/inter/400.css";
-import "@fontsource/inter/700.css";
 import Header from "./Header"
 import Main from "./Main"
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import "@fontsource/inter/400.css";
+import "@fontsource/inter/600.css";
+import "@fontsource/inter/700.css";
 import "./index.css";
 
 const root = document.querySelector("#root");
