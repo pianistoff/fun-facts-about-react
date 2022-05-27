214568bff30f56e55d647f43695a59361dccc18e
pianistoff <severyn.h@gmail.com>
2022-05-27 10:19:41 +0300

    Stylize the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,7 +2,7 @@ import Logo from "./Logo"
 
 export default function Header() {
     return (
-      <header>
+      <header className="header">
         <Logo />
         <h4 className="header-right">React Course - Project 1</h4>
       </header>
